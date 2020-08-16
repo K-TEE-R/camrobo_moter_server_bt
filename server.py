c450ea1f64ba66bfc587ce5b53a36ddcd6e40f7e
kteer <tmngkteer@gmail.com>
2020-08-16 16:05:11 +0100

    changed for bt

--- server.py
+++ server.py
@@ -2,66 +2,50 @@
 # -*- coding:utf-8 -*-
 
 import argparse
-import json
-import SimpleHTTPServer
-import BaseHTTPServer
+import os
+from time import sleep
 
 from driver import Moter
-from time import sleep
 
 left = Moter("Left", 20, 21)
 right= Moter("Right", 27, 22)
 
-def message_handle(message):
-    if message['right'] == 'forward':
+def handle_message(message):
+    if message == 'forward':
         right.forward()
-    elif message['right'] == 'back':
-        right.back()
-    elif message['right'] == 'brake':
-        right.brake()
-    if message['left'] == 'forward':
         left.forward()
-    elif message['left'] == 'back':
+    elif message == 'back':
+        right.back()
         left.back()
-    elif message['left'] == 'brake':
+    elif message =='right':
+        right.brake()
+        left.forward()
+    elif message =='left':
+        right.forward()
         left.brake()
-
-class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def do_POST(self):
-        try:
-            content_len=int(self.headers.get('content-length'))
-            request = json.loads(self.rfile.read(content_len).decode('utf-8'))
-            message_handle(request)
-            response = {'status':200}
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            responseBody = json.dumps(response)
-            self.wfile.write(responseBody.encode('utf-8'))
-        except Exception as e:
-            print("ERROR!!")
-            print(type(e))
-            print(e.args)
-            print(e)
-            response = { 'status' : 500,
-                         'msg' : 'Failed to handle message.' }
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
-            responseBody = json.dumps(response)
-            self.wfile.write(responseBody.encode('utf-8'))
+    elif message =='brake':
+        right.brake()
+        left.brake()
 
 def importargs():
     parser = argparse.ArgumentParser("Processing CAM COMTROLLER server")
-    parser.add_argument('--server_name', '-s', required=False, default='localhost')
-    parser.add_argument('--port', '-p', required=False, type=int, default=8080)
+    parser.add_argument('--port', '-p', required=False, default='/dev/rfcomm0')
     args = parser.parse_args()
-    return args.server_name, args.port
+    return args.port
 
-def run(server_class=BaseHTTPServer.HTTPServer, handler_class=HTTPHandler, server_name='localhost', port=8080):
+def run(port='/dev/rfcomm0'):
+    while True:
+        if os.path.exists(port):
+            print('Found port {}'.format(port))
+            break
+        else:
+            sleep(1)
     try:
-        server = server_class((server_name, port), handler_class)
-        server.serve_forever()
+        with open(port) as p:
+            while True:
+                message = p.readline().strip()
+                print('received message {}'.format(message))
+                handle_message(message)
     except KeyboardInterrupt:
         print("\nCtl+C")
     except Exception as e:
@@ -74,8 +58,8 @@ def run(server_class=BaseHTTPServer.HTTPServer, handler_class=HTTPHandler, serve
 def main():
     right.brake()
     left.brake()
-    server_name, port = importargs()
-    run(server_name=server_name, port=port)
+    port = importargs()
+    run(port=port)
 
 if __name__ == '__main__':
     main()
